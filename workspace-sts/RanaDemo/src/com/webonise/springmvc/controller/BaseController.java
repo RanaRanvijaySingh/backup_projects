5d8158455997eff3b370d4cd3f9e1acadbdc3b9c
webonise <webonise@webonise-desktop.(none)>
2013-07-24 18:17:53 +0530

    look to ranaDemo project on jsp SPRING

---
+++ workspace-sts/RanaDemo/src/com/webonise/springmvc/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.webonise.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class BaseController{
+ 
+   @RequestMapping(method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+
+      return "rana";
+   }
+
+}
+
